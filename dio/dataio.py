34c97029d3d32e08ab741367028093611d40a575
Mike McDermott <defenestrated23@gmail.com>
2016-11-13 11:15:56 -0500

    added label extrator

--- dio/dataio.py
+++ dio/dataio.py
@@ -1,5 +1,7 @@
 from __future__ import print_function, division
+import os
 import numpy as np
+import pandas as pd
 import scipy.io
 
 
@@ -18,3 +20,19 @@ def get_matlab_eeg_data(path):
 
 def get_matlab_eeg_data_ary(path):
     return get_matlab_eeg_data(path)['data']
+
+def reload_with_labels(basename):
+    data = np.load(basename +'.npy')
+    names = pd.read_csv(basename +'_name.csv')
+    label = [os.path.basename(name)[:4]+'_'+name[-5] for name in names['path']]
+    label_ary = []
+    for lab in label:
+        if lab[:3] == 'new':
+            label_ary.append(-1)
+        else:
+            label_ary.append(int(lab[-1]))
+    label_ary = np.array(label_ary).reshape(-1,1)
+    data = np.nan_to_num(np.array(data, float))
+    data_vec = data.reshape(data.shape[0],-1)
+    assert data_vec.shape[0] == label_ary.shape[0], "Shape mismatch with data and label"
+    return (data_vec, label_ary)
