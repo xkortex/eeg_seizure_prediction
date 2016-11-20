a1b26b0389fcb1384e395e1f151a83b0a2c645aa
prefrontalvortex <mikemcdermott23@gmail.com>
2016-11-20 10:53:52 -0500

    more work with the autoencoder

--- models/neural.py
+++ models/neural.py
@@ -1,6 +1,6 @@
 import numpy
-from keras.models import Sequential
-from keras.layers import Dense
+from keras.models import Sequential, Model
+from keras.layers import Dense, Input
 from keras.layers import LSTM
 from keras.layers.convolutional import Convolution1D
 from keras.layers.convolutional import MaxPooling1D
@@ -18,3 +18,49 @@ def make_convo_lstm(n_input_len=256):
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
     return model
+
+
+def autoencoder2(ds, compression_factor=20):
+    # compression_factor=20
+    print('DS shape: {}'.format(ds.shape))
+    in_dims = numpy.prod(ds.shape[1:])
+    encoding_dim = int(in_dims // compression_factor)
+    in_shape = ds[0].shape
+    print('Input Dims: {}, input shape: {}, encoding dims: {}'.format(in_dims, in_shape, encoding_dim))
+
+    # this is our input placeholder
+    input_img = Input(shape=(in_dims,))
+
+    #     encoded = Dense(encoding_dim*4, activation='relu', activity_regularizer=regularizers.activity_l1(10e-5))(input_img)
+    encoded = Dense(encoding_dim * 4, activation='sigmoid')(input_img)
+    encoded = Dense(encoding_dim * 2, activation='sigmoid')(encoded)
+    encoded = Dense(encoding_dim, activation='sigmoid')(encoded)
+
+    # DECODED LAYER
+    # "decoded" is the lossy reconstruction of the input
+    decoded = Dense(encoding_dim * 2, activation='sigmoid')(encoded)
+    decoded = Dense(encoding_dim * 4, activation='sigmoid')(decoded)
+    decoded = Dense(in_dims, activation='sigmoid')(decoded)
+
+    # MODEL
+    autoencoder = Model(input=input_img, output=decoded)
+
+    # SEPERATE ENCODER MODEL
+    encoder = Model(input=input_img, output=encoded)
+
+    # create a placeholder for an encoded (32-dimensional) input
+    encoded_input = Input(shape=(encoding_dim,))
+
+    # retrieve the last layer of the autoencoder model
+    decoder_layer1 = autoencoder.layers[-3]
+    decoder_layer2 = autoencoder.layers[-2]
+    decoder_layer3 = autoencoder.layers[-1]
+
+    # create the decoder model - unrolling the model as we go
+    decoder = Model(input=encoded_input, output=decoder_layer3(decoder_layer2(decoder_layer1(encoded_input))))
+
+    #     model.add(GaussianNoise(0.1), input_shape=(n_input_len,))
+    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+    autoencoder.model_name = 'Autoencoder 1'
+    return autoencoder, encoder, decoder
