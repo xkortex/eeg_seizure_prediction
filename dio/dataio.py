38d4241dff3e200bd1bdeb96ab47abafebf54cfe
prefrontalvortex <mikemcdermott23@gmail.com>
2016-11-19 22:05:44 -0500

    i may be on to something

--- dio/dataio.py
+++ dio/dataio.py
@@ -48,6 +48,7 @@ def separate_sets(data_vec, label_ary):
     """
     # df = pd.DataFrame(data_vec)
     # df['label'] = pd.Series(label_ary)
+    """This part currently breaks on 3D arrays"""
     d0 = data_vec[np.where(label_ary.ravel() == 0)[0], :]
     d1 = data_vec[np.where(label_ary.ravel() == 1)[0], :]
     dt = data_vec[np.where(label_ary.ravel() == -1)[0], :]
@@ -61,8 +62,7 @@ def dump_data(vec_ary, name_ary, meta, filename):
     with open(filename + '.json', 'w') as jfile:
         json.dump(meta, jfile)
 
-
-def subdiv_and_shuffle(data, labels, resample='down', noise=None):
+def subdiv_and_shuffle(data, labels, resample='down', noise=None, merge=True):
     d0, d1, dt = separate_sets(data, labels)
     if resample == 'down':
         np.random.shuffle(d0)
@@ -73,10 +73,36 @@ def subdiv_and_shuffle(data, labels, resample='down', noise=None):
         d1 = np.concatenate([d1, ] * mult, axis=0)
 
     new_set = np.concatenate([d0, d1], axis=0)
-    #     print('new set: ', new_set.shape)
-    new_labels = np.concatenate([np.zeros(len(d0)), np.ones(len(d1))], axis=0).reshape(-1, 1)
+    print('new set: ', new_set.shape)
+    L0, L1 = np.zeros(len(d0)).reshape(-1, 1), np.ones(len(d1)).reshape(-1, 1)
+    print('label shapes: ', L0.shape, L1.shape, len(L0) + len(L1))
+    new_labels = np.concatenate([L0, L1], axis=0).reshape(-1, 1)
     #     print('new labels: ', new_labels.shape)
-    connected_set = np.concatenate([new_set, new_labels], axis=1)
-    np.random.shuffle(connected_set)
-    X, Y = np.array(connected_set[:, :-1]), np.array(connected_set[:, -1:])
+    assert len(new_set) == len(new_labels), "Something failed, lengths of X and Y are not the same"
+    if merge:
+        connected_set = np.concatenate([new_set, new_labels], axis=1)
+        np.random.shuffle(connected_set)
+        X, Y = np.array(connected_set[:, :-1]), np.array(connected_set[:, -1:])
+
+    else:
+        # raise NotImplementedError('Still need to build a sensible shuffler for multi-dim data')
+        fullidx = np.arange(new_set.shape[0])
+        np.random.shuffle(fullidx)
+        shuf_x = []
+        shuf_y = [] # inefficient i know but it should be reliable
+        for j in fullidx:
+            sj = fullidx[j] # shuffled index
+            shuf_x.append(new_set[sj])
+            shuf_y.append(new_labels[sj])
+            X = np.array(shuf_x)
+            Y = np.array(shuf_y)
+
     return X, Y
+
+def deinterleave(data, nchan=16):
+    """
+    Converts 3D array with channel as last dim, to serialized (one channel after another) feature vector
+    :param data:
+    :param nchan:
+    :return:
+    """
