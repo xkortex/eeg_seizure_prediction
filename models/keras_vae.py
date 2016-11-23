b20598b0820950cb67fd1f8f02f058c150bf5395
prefrontalvortex <mikemcdermott23@gmail.com>
2016-11-23 16:48:25 -0500

    update to convae class

--- models/keras_vae.py
+++ models/keras_vae.py
@@ -5,7 +5,8 @@ Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6114
 import numpy as np
 import matplotlib.pyplot as plt
 
-from keras.layers import Input, Dense, Lambda
+from keras.layers import Input, Dense, Lambda, Flatten, Reshape
+from keras.layers import Convolution2D, Deconvolution2D
 from keras.models import Model
 from keras import backend as K
 from keras import objectives
@@ -13,7 +14,6 @@ from keras.datasets import mnist
 
 # global
 nb_epoch = 50
-
 class VariationalAutoencoder(object):
     def __init__(self, original_dim=784, latent_dim=2, intermediate_dim=256, batch_size=100, epsilon_std=1.0):
         #vae params
@@ -23,17 +23,17 @@ class VariationalAutoencoder(object):
         self.intermediate_dim = intermediate_dim
         self.epsilon_std = epsilon_std
 
-        x = Input(batch_shape=(batch_size, original_dim))
-        h = Dense(intermediate_dim, activation='relu')(x)
-        self.z_mean = Dense(latent_dim)(h)
-        self.z_log_var = Dense(latent_dim)(h)
+        x = Input(batch_shape=(batch_size, original_dim), name='x_input')
+        h = Dense(intermediate_dim, activation='relu', name='h_hidden_relu_1')(x)
+        self.z_mean = Dense(latent_dim, name='Z_Mean')(h)
+        self.z_log_var = Dense(latent_dim, name='Z_Log_Var')(h)
 
         # note that "output_shape" isn't necessary with the TensorFlow backend
         z = Lambda(self.sampling, output_shape=(latent_dim,))([self.z_mean, self.z_log_var])
 
         # we instantiate these layers separately so as to reuse them later
-        decoder_h = Dense(intermediate_dim, activation='relu')
-        decoder_mean = Dense(original_dim, activation='sigmoid')
+        decoder_h = Dense(intermediate_dim, activation='relu', name='Decoder_H_Relu')
+        decoder_mean = Dense(original_dim, activation='sigmoid', name='Decoder_Mean_sig')
         h_decoded = decoder_h(z)
         x_decoded_mean = decoder_mean(h_decoded)
 
@@ -57,62 +57,208 @@ class VariationalAutoencoder(object):
         return xent_loss + kl_loss
 
 
-    def sampling(self, args):
-        z_mean, z_log_var = args
+    def sampling(self, z_args):
+        "Unpacks the tuple input and conducts probabilistic sampling"
+        z_mean, z_log_var = z_args
         epsilon = K.random_normal(shape=(self.batch_size, self.latent_dim), mean=0.,
                                   std=self.epsilon_std)
         return z_mean + K.exp(z_log_var / 2) * epsilon
 
     def fit(self, x, y, batch_size=None, nb_epoch=10, verbose=1, callbacks=[], validation_split=0.,
             validation_data=None, shuffle=True, class_weight=None, sample_weight=None):
-        pass
+        callbacks_history = self.model.fit(x, y, batch_size, nb_epoch, verbose, callbacks, validation_split,
+                                           validation_data, shuffle, class_weight, sample_weight)
+        return callbacks_history
 
 
-vaeclass = VariationalAutoencoder()
-vae = vaeclass.model
+class ConvoVAE(object):
+    '''This script demonstrates how to build a variational autoencoder
+    with Keras and deconvolution layers.
 
-# ==== dataset handling - train the VAE on MNIST digits ===
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6114
+    '''
+    def __init__(self, input_shape=(28,28), latent_dim=2, intermediate_dim=256, batch_size=100, epsilon_std=1.0):
+        # input image dimensions
+        self.input_shape = input_shape
+        if len(input_shape) == 3:
+            self.img_rows, self.img_cols, self.img_chns = input_shape
+        elif len(input_shape) == 2:
+            self.img_rows, self.img_cols = input_shape
+            self.img_chns = 1
+        else:
+            raise IndexError("Invalid shape: {}".format(input_shape))
+        self.batch_size = batch_size
+        self.original_dim = np.prod(input_shape)
+        self.latent_dim = latent_dim
+        self.intermediate_dim = intermediate_dim
+        self.epsilon_std = epsilon_std
 
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+        # number of convolutional filters to use
+        nb_filters = 64
+        # convolution kernel size
+        nb_conv = 3
+
+        batch_size = 100
+        if K.image_dim_ordering() == 'th':
+            original_img_size = (self.img_chns, self.img_rows, self.img_cols)
+        else:
+            original_img_size = (self.img_rows, self.img_cols, self.img_chns)
+        latent_dim = 2
+        intermediate_dim = 128
+        epsilon_std = 1.0
+        nb_epoch = 5
+
+        x = Input(batch_shape=(batch_size,) + original_img_size)
+        conv_1 = Convolution2D(self.img_chns, 2, 2, border_mode='same', activation='relu')(x)
+        conv_2 = Convolution2D(nb_filters, 2, 2,
+                               border_mode='same', activation='relu', subsample=(2, 2))(conv_1)
+        conv_3 = Convolution2D(nb_filters, nb_conv, nb_conv,
+                               border_mode='same', activation='relu', subsample=(1, 1))(conv_2)
+        conv_4 = Convolution2D(nb_filters, nb_conv, nb_conv,
+                               border_mode='same', activation='relu', subsample=(1, 1))(conv_3)
+        flat = Flatten()(conv_4)
+        hidden = Dense(intermediate_dim, activation='relu')(flat)
+
+        z_mean = Dense(latent_dim)(hidden)
+        z_log_var = Dense(latent_dim)(hidden)
+
+        # note that "output_shape" isn't necessary with the TensorFlow backend
+        # so you could write `Lambda(sampling)([z_mean, z_log_var])`
+        z = Lambda(self.sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+        # we instantiate these layers separately so as to reuse them later
+        decoder_hid = Dense(intermediate_dim, activation='relu')
+        decoder_upsample = Dense(nb_filters * 14 * 14, activation='relu')
+
+        if K.image_dim_ordering() == 'th':
+            output_shape = (batch_size, nb_filters, 14, 14)
+        else:
+            output_shape = (batch_size, 14, 14, nb_filters)
+
+        decoder_reshape = Reshape(output_shape[1:])
+        decoder_deconv_1 = Deconvolution2D(nb_filters, nb_conv, nb_conv, output_shape,
+                                           border_mode='same', subsample=(1, 1), activation='relu')
+        decoder_deconv_2 = Deconvolution2D(nb_filters, nb_conv, nb_conv, output_shape,
+                                           border_mode='same', subsample=(1, 1),activation='relu')
+        if K.image_dim_ordering() == 'th':
+            output_shape = (batch_size, nb_filters, 29, 29)
+        else:
+            output_shape = (batch_size, 29, 29, nb_filters)
+        decoder_deconv_3_upsamp = Deconvolution2D(nb_filters, 2, 2, output_shape,
+                                                  border_mode='valid', subsample=(2, 2), activation='relu')
+        decoder_mean_squash = Convolution2D(self.img_chns, 2, 2, border_mode='valid', activation='sigmoid')
+
+        hid_decoded = decoder_hid(z)
+        up_decoded = decoder_upsample(hid_decoded)
+        reshape_decoded = decoder_reshape(up_decoded)
+        deconv_1_decoded = decoder_deconv_1(reshape_decoded)
+        deconv_2_decoded = decoder_deconv_2(deconv_1_decoded)
+        x_decoded_relu = decoder_deconv_3_upsamp(deconv_2_decoded)
+        x_decoded_mean_squash = decoder_mean_squash(x_decoded_relu)
+
+        self.model = Model(x, x_decoded_mean_squash)
+        self.model.compile(optimizer='rmsprop', loss=self.vae_loss)
+        # self.model.summary()
+
+        # build a model to project inputs on the latent space
+        self.encoder = Model(x, z_mean)
+
+        # build a digit generator that can sample from the learned distribution
+        # todo: (un)roll this
+        decoder_input = Input(shape=(latent_dim,))
+        _hid_decoded = decoder_hid(decoder_input)
+        _up_decoded = decoder_upsample(_hid_decoded)
+        _reshape_decoded = decoder_reshape(_up_decoded)
+        _deconv_1_decoded = decoder_deconv_1(_reshape_decoded)
+        _deconv_2_decoded = decoder_deconv_2(_deconv_1_decoded)
+        _x_decoded_relu = decoder_deconv_3_upsamp(_deconv_2_decoded)
+        _x_decoded_mean_squash = decoder_mean_squash(_x_decoded_relu)
+        self.generator = Model(decoder_input, _x_decoded_mean_squash)
+
+    def sampling(self, args):
+        z_mean, z_log_var = args
+        epsilon = K.random_normal(shape=(self.batch_size, self.latent_dim),
+                                  mean=0., std=self.epsilon_std)
+        return z_mean + K.exp(z_log_var) * epsilon
+
+    def vae_loss(self, x, x_decoded_mean):
+        # NOTE: binary_crossentropy expects a batch_size by dim
+        # for x and x_decoded_mean, so we MUST flatten these!
+        x = K.flatten(x)
+        x_decoded_mean = K.flatten(x_decoded_mean)
+        xent_loss = self.img_rows * self.img_cols * objectives.binary_crossentropy(x, x_decoded_mean)
+        kl_loss = - 0.5 * K.mean(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
+        return xent_loss + kl_loss
+
+    def fit(self, x, y, batch_size=None, nb_epoch=10, verbose=1, callbacks=[], validation_split=0.,
+            validation_data=None, shuffle=True, class_weight=None, sample_weight=None):
+        callbacks_history = self.model.fit(x, y, batch_size, nb_epoch, verbose, callbacks, validation_split,
+                                           validation_data, shuffle, class_weight, sample_weight)
+        return callbacks_history
 
 
-vae.fit(x_train, x_train,
-        shuffle=True,
-        nb_epoch=nb_epoch,
-        batch_size=batch_size,
-        validation_data=(x_test, x_test))
+
+def train_convovae(original_img_size):
+    # train the VAE on MNIST digits
+    (x_train, _), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.astype('float32') / 255.
+    x_train = x_train.reshape((x_train.shape[0],) + original_img_size)
+    x_test = x_test.astype('float32') / 255.
+    x_test = x_test.reshape((x_test.shape[0],) + original_img_size)
+
+    print('x_train.shape:', x_train.shape)
 
 
-# ===== plotting encoder output
-# display a 2D plot of the digit classes in the latent space
-x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
-plt.figure(figsize=(6, 6))
-plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
-plt.colorbar()
-plt.show()
+def vae_process():
+    vaeclass = VariationalAutoencoder()
+    vae = vaeclass.model
+    encoder = vaeclass.encoder
+    generator = vaeclass.generator
+
+    # ==== dataset handling - train the VAE on MNIST digits ===
+    batch_size = 100
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
 
-# ====== plotting decoder from latent space =======
-# display a 2D manifold of the digits
-n = 15  # figure with 15x15 digits
-digit_size = 28
-figure = np.zeros((digit_size * n, digit_size * n))
-# we will sample n points within [-15, 15] standard deviations
-grid_x = np.linspace(-15, 15, n)
-grid_y = np.linspace(-15, 15, n)
+    vae.fit(x_train, x_train,
+            shuffle=True,
+            nb_epoch=nb_epoch,
+            batch_size=batch_size,
+            validation_data=(x_test, x_test))
 
-for i, yi in enumerate(grid_x):
-    for j, xi in enumerate(grid_y):
-        z_sample = np.array([[xi, yi]])
-        x_decoded = generator.predict(z_sample)
-        digit = x_decoded[0].reshape(digit_size, digit_size)
-        figure[i * digit_size: (i + 1) * digit_size,
-               j * digit_size: (j + 1) * digit_size] = digit
 
-plt.figure(figsize=(10, 10))
-plt.imshow(figure)
-plt.show()
+    # ===== plotting encoder output
+    # display a 2D plot of the digit classes in the latent space
+    x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+    plt.figure(figsize=(6, 6))
+    plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
+    plt.colorbar()
+    plt.show()
+
+
+    # ====== plotting decoder from latent space =======
+    # display a 2D manifold of the digits
+    n = 15  # figure with 15x15 digits
+    digit_size = 28
+    figure = np.zeros((digit_size * n, digit_size * n))
+    # we will sample n points within [-15, 15] standard deviations
+    grid_x = np.linspace(-15, 15, n)
+    grid_y = np.linspace(-15, 15, n)
+
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = generator.predict(z_sample)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[i * digit_size: (i + 1) * digit_size,
+                   j * digit_size: (j + 1) * digit_size] = digit
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(figure)
+    plt.show()
