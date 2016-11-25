493dac36e326e6d943192e782523cf2f02ef13d8
prefrontalvortex <mikemcdermott23@gmail.com>
2016-11-24 23:45:49 -0500

    updated crossfire model python main code

--- models/crossfire.py
+++ models/crossfire.py
@@ -21,7 +21,7 @@ class Crossfire(object):
     * Modularize out the activation from dropout ordering"""
 
     def __init__(self, input_shape=(28, 28, 1), latent_dim=2, intermediate_dim=256, batch_size=100, epsilon_std=1.0,
-                 dropout_p=0.1, n_stax=1, n_classes=10):
+                 dropout_p=0.1, n_stax=0, n_classes=10):
         # input image dimensions
         self.input_shape = input_shape
         if len(input_shape) == 3:
@@ -36,6 +36,7 @@ class Crossfire(object):
         self.latent_dim = latent_dim
         self.intermediate_dim = intermediate_dim
         self.epsilon_std = epsilon_std
+        self.epsilon_ce = 1.0e-9
 
         # number of convolutional filters to use
         nb_filters = 64
@@ -48,7 +49,7 @@ class Crossfire(object):
         else:
             self.original_img_size = (self.img_rows, self.img_cols, self.img_chns)
 
-        x = Input(batch_shape=(batch_size,) + self.original_img_size)
+        x = Input(batch_shape=(batch_size,) + self.original_img_size, name='main_input')
         conv_1 = Convolution2D(self.img_chns, 2, 2, border_mode='same', activation='relu')(x)
         conv_2 = Convolution2D(nb_filters, 2, 2,
                                border_mode='same', activation='relu', subsample=(2, 2))(conv_1)
@@ -75,11 +76,15 @@ class Crossfire(object):
 
         ## ==== End of encoding portion ======
 
-        # todo: I think I might need more linear in between the convo and latent
-
         ## ==== Crossfire classifier =========
         #         c = Lambda(self.crosser, output_shape=(latent_dim,))(self.z_mean, self.z_log_var)
         classer = Dense(n_classes, init='normal', activation='softmax', name='classer')(self.z_mean)
+
+        # Slicer example
+        # y = Lambda(lambda x: x[:,0,:,:], output_shape=(1,) + input_shape[2:])(x)
+
+
+
 
         # we instantiate these layers separately so as to reuse them later
         decoder_hid = Dense(intermediate_dim, activation='relu')
@@ -101,7 +106,8 @@ class Crossfire(object):
             output_shape = (batch_size, 29, 29, nb_filters)
         decoder_deconv_3_upsamp = Deconvolution2D(nb_filters, 2, 2, output_shape,
                                                   border_mode='valid', subsample=(2, 2), activation='relu')
-        decoder_mean_squash = Convolution2D(self.img_chns, 2, 2, border_mode='valid', activation='sigmoid')
+        decoder_mean_squash = Convolution2D(self.img_chns, 2, 2, border_mode='valid',
+                                            activation='sigmoid', name='main_output')
 
         hid_decoded = decoder_hid(z)
         up_decoded = decoder_upsample(hid_decoded)
@@ -125,16 +131,31 @@ class Crossfire(object):
         # Generate models
         # Primary model - VAE
         self.model = Model(x, x_decoded_mean_squash)
-        # Crossfile network
+        # Crossfilre network
         self.classifier = Model(x, classer)
+        # Ok, now comes the tricky part. See these references:
+        # https://keras.io/getting-started/functional-api-guide/#multi-input-and-multi-output-models
+        self.crossmodel = Model(input=x, output=[x_decoded_mean_squash, classer])
+        self.crossmodel.compile(optimizer='rmsprop',
+                                loss={'main_output': self.vae_loss,
+                                      'classer': 'categorical_crossentropy'},
+                                loss_weights={'main_output': 1.0,
+                                              'classer': 1.0})
 
         # build a model to project inputs on the latent space
         self.encoder = Model(x, self.z_mean)
         # reconstruct digits from latent space
         self.generator = Model(decoder_input, _x_decoded_mean_squash)
         self.model.compile(optimizer='rmsprop', loss=self.vae_loss)
-        self.classifier.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        #         self.classifier.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        self.classifier.compile(loss=self.custom_crossent, optimizer='adam', metrics=['accuracy'])
 
+    def fit_crossmodel(self, x_dict, y_dict, batch_size=None, nb_epoch=10, verbose=1, callbacks=[], validation_split=0.,
+                       validation_data=None, shuffle=True, class_weight=None, sample_weight=None):
+        callbacks_history = self.crossmodel.fit(x_dict, y_dict, batch_size, nb_epoch, verbose, callbacks,
+                                                validation_split,
+                                                validation_data, shuffle, class_weight, sample_weight)
+        return callbacks_history
 
     def sampling(self, args):
         z_mean, z_log_var = args
@@ -152,8 +173,17 @@ class Crossfire(object):
         x = K_backend.flatten(x)
         x_decoded_mean = K_backend.flatten(x_decoded_mean)
         xent_loss = self.img_rows * self.img_cols * objectives.binary_crossentropy(x, x_decoded_mean)
-        kl_loss = - 0.5 * K_backend.mean(1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var), axis=-1)
+        kl_loss = - 0.5 * K_backend.mean(
+            1 + self.z_log_var - K_backend.square(self.z_mean) - K_backend.exp(self.z_log_var), axis=-1)
         return xent_loss + kl_loss
+
+    def custom_crossent(self, y_true, y_pred):
+        '''Just another crossentropy'''
+
+        #         y_pred = K_backend.clip(y_pred, self.epsilon_ce, 1.0 - self.epsilon_ce)
+        #         y_pred /= K_backend.sum(y_pred, axis=-1, keepdims=True)
+        cce = K_backend.categorical_crossentropy(y_pred, y_true)
+        return cce
 
     def fit(self, x, y, batch_size=None, nb_epoch=10, verbose=1, callbacks=[], validation_split=0.,
             validation_data=None, shuffle=True, class_weight=None, sample_weight=None):
@@ -167,16 +197,14 @@ class Crossfire(object):
                                            validation_data, shuffle, class_weight, sample_weight)
         return callbacks_history
 
-    def crossfit(self, x, y, batch_size=None, nb_epoch=10, verbose=1, callbacks=[], validation_split=0.,
+    def crossfit(self, x, y, batch_size=None, nb_epoch_ae=5, nb_epoch_cf=10, verbose=1, callbacks=[],
+                 validation_split=0.,
                  validation_data=None, shuffle=True, class_weight=None, sample_weight=None):
-        """ Note: I found that during full-epoch cross-traning, after a cycle or two, the error goes to NaN. I think
-        the loss is exploding w.r.t. VAE after the classifier pass. Will most likely need to use sub-epochs or ideally,
-        combine the loss function into a single metric.
-        """
-        for i in range(nb_epoch):
+        valid_ae, valid_cf = validation_data
+        for i in range(nb_epoch_ae):
             callbacks_history = self.model.fit(x, x, batch_size, 1, verbose, callbacks, validation_split,
-                                               validation_data, shuffle, class_weight, sample_weight)
-
+                                               valid_ae, shuffle, class_weight, sample_weight)
+        for i in range(nb_epoch_cf):
             callbacks_history = self.classifier.fit(x, y, batch_size, 1, verbose, callbacks, validation_split,
-                                                    validation_data, shuffle, class_weight, sample_weight)
+                                                    valid_cf, shuffle, class_weight, sample_weight)
         return callbacks_history
